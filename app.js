1d616ccdb643eae527a3652db55d0c2b7726457d
amroaltai <amro.altai@hotmail.com>
2025-01-12 23:09:02 +0100

    Fixade så att man kan omredigeras till söksidan när man söker i sökfältet och klickar på enter, la till headern, förberedde grid där produkterna kommer visas när dom finns i databasen, kopplade mig till databasen, fixade så att man kan söka på produkters namn o få resultat efter det, samt om inga produkter hittas så visas en text längst upp

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRoute = require('./routes/products,db.js');
 var contactRouter = require('./routes/contact');
+var searchRouter = require("./routes/search-page");
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(productsRoute);
 app.use('/contact', contactRouter);
+app.use(searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
