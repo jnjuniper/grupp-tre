988d962a6073f1bec0998de3a1fee1aca0d57cc7
Oliver Borys <oliver.borys@gmail.com>
2025-01-18 11:39:44 +0100

    Added title to about

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const aboutContent = require('../data/about.js');
 
 router.get('/', (req, res) => {
-  res.render('about', { aboutContent }); 
+  res.render('about', { title: 'Om oss', aboutContent }); 
 });
 
 module.exports = router; 
