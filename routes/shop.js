d08a4abc6b7df4ba385606c7cddc01f7e9d31db1
amroaltai <amro.altai@hotmail.com>
2025-01-23 23:29:32 +0100

    Nu är det samma sortering på produktsidan som på söksidan

--- routes/shop.js
+++ routes/shop.js
@@ -2,23 +2,26 @@ var express = require('express');
 var router = express.Router();
 var Database = require('better-sqlite3');
 
-const db = new Database('./db/products.db'); 
+const db = new Database('./db/products.db');
 
 router.get('/', (req, res) => {
     try {
-        const sort = req.query.sort || 'az'; 
+        const sort = req.query.sort || 'newest'; 
         let query = 'SELECT * FROM products';
 
-
+        
         if (sort === 'highest') {
             query += ' ORDER BY price DESC';
         } else if (sort === 'lowest') {
             query += ' ORDER BY price ASC';
-        } else if (sort === 'az') {
-            query += ' ORDER BY productName COLLATE NOCASE ASC';
+        } else if (sort === 'newest') {
+            query += ' ORDER BY id DESC'; 
+        } else if (sort === 'oldest') {
+            query += ' ORDER BY id ASC'; 
         }
-
+     
         const products = db.prepare(query).all();
+
 
         res.render('shop', {
             title: 'Shop',
