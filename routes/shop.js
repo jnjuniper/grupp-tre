814102f9f5b8ef7b14ee6345cf5fc88dec535733
amroaltai <amro.altai@hotmail.com>
2025-01-23 22:46:58 +0100

    Created drop down button on the shop page, sort from A-Z and lowest price to highest

--- routes/shop.js
+++ routes/shop.js
@@ -2,20 +2,33 @@ var express = require('express');
 var router = express.Router();
 var Database = require('better-sqlite3');
 
-const db = new Database('./db/products.db'); // Adjust the path to your database file
+const db = new Database('./db/products.db'); 
 
 router.get('/', (req, res) => {
     try {
-      const products = db.prepare('SELECT * FROM products').all(); // Fetch 8 products
-      res.render('shop', {
-        title: 'Shop',
-        products,
-      });
-    } catch (error) {
-      console.error('Error fetching products:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
+        const sort = req.query.sort || 'az'; 
+        let query = 'SELECT * FROM products';
 
-  
+
+        if (sort === 'highest') {
+            query += ' ORDER BY price DESC';
+        } else if (sort === 'lowest') {
+            query += ' ORDER BY price ASC';
+        } else if (sort === 'az') {
+            query += ' ORDER BY productName COLLATE NOCASE ASC';
+        }
+
+        const products = db.prepare(query).all();
+
+        res.render('shop', {
+            title: 'Shop',
+            products,
+            sort,
+        });
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 module.exports = router;
